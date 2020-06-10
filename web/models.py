63ee05448bea5dc449c44f9a9ee102c94a85ee77
3q841 <comtech9064@gmail.com>
2020-06-10 21:32:33 +0430

    set __str__ name for post title

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class post(models.Model):
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='xreenshot', default='')
+    def __str__(self):
+        return self.auther
