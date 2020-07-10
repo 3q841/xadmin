eee8a47f3de4677fa0193f372c7c6efc16d574a0
3q841 <comtech9064@gmail.com>
2020-07-10 20:48:59 +0430

    rm png file from media dir

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 #from .views import PostListView
 from . import views
-from django.conf import settings
+#from django.conf import settings
 
 urlpatterns = [
     path('blog/exp/', views.sub_qustion, name='user-ans'),
     path('accounts/register/', views.register, name = 'register'),
     path('', views.index , name='index'),
 ]
-
+"""
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)"""
